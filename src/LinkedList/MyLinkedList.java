d6b6a80657f13662a998bc9422412b35a0fc9590
kanishk <kanishkbharadwaj333@gmail.com>
2020-06-15 01:01:03 +0530

    added getNode() method

--- src/LinkedList/MyLinkedList.java
+++ src/LinkedList/MyLinkedList.java
@@ -1,4 +1,43 @@
 package LinkedList;
 
-public class MyLinkedList {
+import java.rmi.MarshalledObject;
+
+public class MyLinkedList<E> {
+    private Node<E> head = null;
+    private int size = 0;
+
+    private Node<E> getNode(int index){
+        Node<E> response = head;
+
+        for (int i = 0; i <  index; i++) {
+            response = response.getNext();
+        }
+    return response;
+    }
+
+    void push( E item){
+
+    }
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+            next = null;
+        }
+        public Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext(){
+            return next;
+        }
+    }
 }
