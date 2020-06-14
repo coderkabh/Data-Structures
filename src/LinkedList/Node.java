56e8a26671a2b5576eeb15df9b53759441c53b39
kanishk <kanishkbharadwaj333@gmail.com>
2020-06-15 00:35:30 +0530

    added node class initial code 2

---
+++ src/LinkedList/Node.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class Node<E> {
+    E data;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+}
+
+
